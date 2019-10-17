5c3db9da4b0a69f9829d6dae9fecd71135c2cdd0
Paweł Oleniuk <nelsoleworld@gmail.com>
2019-10-17 11:15:43 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name;
+            Console.WriteLine("Podaj swoje Imię: ");
+            name = Console.ReadLine();
+            Console.WriteLine(name + name);
+            Console.ReadKey();
+        }
+    }
+}
